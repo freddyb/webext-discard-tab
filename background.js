1c68a3fa491649d01a84bf67b1e5cffb57513159
Christian <me@rndm.de>
2018-02-21 19:41:09 +0100

    fix: remove unsupported usage of updating a tab title

--- background.js
+++ background.js
@@ -1,6 +1,4 @@
 "use strict";
-const SNOWFLAKE = "\u2744 ️";
-
 
 browser.menus.create({
   id: "discard",
@@ -9,10 +7,6 @@ browser.menus.create({
   contexts: ["tab"]
 });
 
-// eslint fails to realize that the param of an arrow function cant be blank/undefined, so ignore warning below
-// eslint-disable-next-line no-unused-vars
 browser.menus.onClicked.addListener((info, tab) => {
    browser.tabs.discard(tab.id);
-   browser.tabs.update(tab.id, { title: SNOWFLAKE + tab.title });
-
 });
