8a45c692951417413f3ef8bc53c8a66a3d4d9943
Rob Wu <rob@robwu.nl>
2018-11-26 23:04:55 +0100

    fix: Avoid <all_urls> permission if possible
    This is a follow-up to https://github.com/freddyb/webext-discard-tab/pull/7#issuecomment-374232978

--- options.js
+++ options.js
@@ -1,9 +1,22 @@
 "use strict";
 
+function requiresPermission() {
+  // As of Firefox 63, the "activeTab" unlocks access to the tab that was
+  // clicked ( https://bugzilla.mozilla.org/show_bug.cgi?id=1446956 ).
+  // Support for this functionality cannot easily be detected, so indirectly
+  // infer support for this feature via a different change in the same release:
+  // https://developer.mozilla.org/en-US/docs/Mozilla/Firefox/Releases/63#Menus
+
+  // If we ever stop supporting Firefox 62 and earlier (including ESR 60), then
+  // the "<all_urls>" optional permission can be removed from manifest.json,
+  // together with the uses of the browser.permissions API in this file.
+  return !browser.menus.getTargetElement;
+}
+
 var permissions = { origins: ["<all_urls>"] };
 var prefPrependSnowflake = document.getElementById("prefPrependSnowflake");
 prefPrependSnowflake.onchange = async () => {
-  if (prefPrependSnowflake.checked) {
+  if (prefPrependSnowflake.checked && requiresPermission()) {
     prefPrependSnowflake.disabled = true;
     // Note: permissions.request only works when this page is shown in a tab,
     // i.e. when options_ui.open_in_tab=true in manifest.json.
